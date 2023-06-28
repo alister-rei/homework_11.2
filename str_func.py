43c23f351ba7b8a957deaa896afb758a01ab6efd
Хадков_Алексей <lhadkov@mail.ru>
2023-06-28 21:06:43 +0300

    new function foo_2

--- str_func.py
+++ str_func.py
@@ -1,3 +1,16 @@
 def foo(string):
+    '''
+    docstring
+    :param string:
+    :return:
+    '''
     return string.upper()
 
+
+def foo_2(string):
+    '''
+    docstring
+    :param string:
+    :return:
+    '''
+    return string.title()
